095aff8aa6c4a7b3ebbd1627a6fe29ec3ad2fe05
John Reyes <john.jreye@gmail.com>
2019-08-16 20:05:00 +0000

    Done.

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,23 @@
 def addition(num1, num2)
-
+5 + 4
 end
 
 def subtraction(num1, num2)
-
+8 - 3
 end
 
 def division(num1, num2)
-
+100 / 4
 end
 
 def multiplication(num1, num2)
-
+60 * 2
 end
 
 def modulo(num1, num2)
-
+24 % 5
 end
 
 def square_root(num)
-
+Math.sqrt(81)
 end
